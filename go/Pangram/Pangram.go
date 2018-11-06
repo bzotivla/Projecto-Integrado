1ce163a9c8d9399c8bd9991a99a6b3b35a4f7760
pedrompnunes <32069885+pedrompnunes@users.noreply.github.com>
2018-11-06 17:25:02 +0000

    Create Pangram.go

---
+++ go/Pangram/Pangram.go
@@ -0,0 +1,17 @@
+package pangram
+
+import "strings"
+
+func IsPangram(s string) bool {
+    letters := map[rune]bool{}
+    for _, letter := range strings.ToLower(s) {
+        letters[letter] = true
+    }
+    for _, l := range  "abcdefghijklmnopqrstuvwxyz" {
+        if _, exists := letters[l]; !exists {
+            return false
+        }
+    }
+    return true
+}
+© 2018 GitHub, Inc.
